0a3a8fca79e40f167db8abbbe813a8e77f1afef0
mr-evil1 <vavootopro@gmail.com>
2026-07-10 07:33:22 +0200

    Add user agent string for web scraping

--- scrapers/filmpalast.py
+++ scrapers/filmpalast.py
@@ -8,6 +8,7 @@ SITE_DOMAIN  = 'filmpalast.to'
 TYPE         = 'both'
 GLOBAL_SEARCH = True
 
+
 _UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
 
 
